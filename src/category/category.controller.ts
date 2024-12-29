2b702c7e435d0edca93bd4b70c3633c13d832757
SecondSocks <159525526+SecondSocks@users.noreply.github.com>
2024-12-30 00:39:56 +0700

    Develop category entity

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,7 +1,60 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller, Delete,
+  Get, HttpCode,
+  Param, Post,
+  Put,
+  UsePipes,
+  ValidationPipe
+} from '@nestjs/common'
+import { UseAuth } from '../auth/decorators/auth.decorator'
+import { CurrentUser } from '../auth/decorators/user.decorator'
 import { CategoryService } from './category.service';
+import { CategoryDto } from './dto/category.dto'
 
-@Controller('category')
+@Controller('categories')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
+  
+  @Get()
+  async getAll() {
+    return this.categoryService.getAll()
+  }
+  
+  @Get('by-slug/:slug')
+  async getBySlug(@Param('slug') slug: string) {
+    return this.categoryService.getBySlug(slug)
+  }
+  
+  @Get(':id')
+  @UseAuth()
+  async getById(@Param('id') categoryId: string) {
+    return this.categoryService.getById(categoryId)
+  }
+  
+  @UsePipes(new ValidationPipe())
+  @UseAuth()
+  @HttpCode(200)
+  @Put(':id')
+  async updateCategory(
+    @Param('id') categoryId: string,
+    @Body() dto: CategoryDto
+  ) {
+    return this.categoryService.updateCategory(categoryId, dto)
+  }
+  
+  @UseAuth()
+  @HttpCode(200)
+  @Post()
+  async createCategory(
+  ) {
+    return this.categoryService.createCategory()
+  }
+  
+  @UseAuth()
+  @HttpCode(200)
+  @Delete(':id')
+  async deleteCategory(@Param('id') categoryId: string) {
+    return this.categoryService.deleteCategory(categoryId)
+  }
 }
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { PrismaService } from '../prisma.service'
+import { generatingSlug } from '../utils/generate-slug'
+import { CategoryDto } from './dto/category.dto'
+import { returnCategoryObject } from './return-category.object'
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+	constructor(private readonly prisma: PrismaService) {}
+	
+	async getById(id: string) {
+		const category = await this.prisma.category.findUnique({
+			where: { id },
+			select: returnCategoryObject
+		})
+		
+		if (!category) throw new NotFoundException('Category not found')
+		
+		return category
+	}
+	
+	async getBySlug(slug: string) {
+		const category = await this.prisma.category.findUnique({
+			where: { slug },
+			select: returnCategoryObject
+		})
+		
+		if (!category) throw new NotFoundException('Category not found')
+		
+		return category
+	}
+	
+	async getAll() {
+		return this.prisma.category.findMany({
+			select: returnCategoryObject
+		})
+	}
+	
+	async createCategory() {
+		return this.prisma.category.create({
+			data: {
+				name: '',
+				slug: ''
+			}
+		})
+	}
+	
+	async updateCategory(id: string, dto: CategoryDto) {
+		return this.prisma.category.update({
+			where: { id },
+			data: {
+				name: dto.name,
+				slug: generatingSlug(dto.name)
+			}
+		})
+	}
+	
+	async deleteCategory(id: string) {
+		return this.prisma.category.delete({
+			where: { id }
+		})
+	}
+}
